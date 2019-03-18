d4237354b4b071ed812b626282bd06c1d0e01224
hejiyun <2879722585@qq.com>
2019-03-18 16:28:39 +0800

    登陆验证初步完成

---
+++ public/admin/js/login.js
@@ -0,0 +1,59 @@
+$(function(){
+    var $form = $('#form')
+    $form.bootstrapValidator({
+      fields: {
+        username: {
+          validators: {
+            notEmpty: {
+              message: '用户名不能为空'
+            },
+            stringLength: {
+              min: 3,
+              max: 9,
+              message: '用户名长度必须是3-9位'
+            }
+          }
+        },
+        password: {
+          validators: {
+            notEmpty: {
+              message: '用户密码不能为空'
+            },
+            stringLength: {
+              min: 6,
+              max: 12,
+              message: '用户密码长度必须是6-12位'
+            }
+          }
+        }
+      },
+      feedbackIcons: {
+        valid: 'glyphicon glyphicon-thumbs-up',
+        invalid: 'glyphicon glyphicon-thumbs-down',
+        validating: 'glyphicon glyphicon-refresh'
+      }
+    })
+    $form.on('success.form.bv', function (e) {
+      e.preventDefault()
+      $.ajax({
+        type: 'post',
+        url: '/employee/employeeLogin',
+        data: $form.serialize(),
+        dataType: 'json',
+        success: function (info) {
+          if (info.error === 1000) {
+              alert('用户名错误')
+          }
+          if (info.error === 1001) {
+            alert('密码错误')
+          }
+          if (info.success) {
+            location.href = 'index.html'
+          }
+        }
+      })
+    })
+    $('[type="reset"]').click(function() {
+      $('#form').data("bootstrapValidator").resetForm();
+    })
+  })
