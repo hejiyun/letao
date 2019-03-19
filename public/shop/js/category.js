6467f87def65e581ddab75b59f117597149b1aad
hejiyun <2879722585@qq.com>
2019-03-19 16:54:38 +0800

    前台分类页面完成

---
+++ public/shop/js/category.js
@@ -0,0 +1,47 @@
+
+
+$(function () {
+
+  $.ajax({
+      type:'get',
+      url:'/category/queryTopCategory',
+      dataType:'json',
+      success: function (info) {
+          var htmlstr = template('fl',info)
+          $('.changebg').html(htmlstr)
+      }
+  })
+
+  
+  $.ajax({
+    type:'get',
+    url:'/category/querySecondCategory',
+    dataType:'json',
+    data:{
+        id: 1
+    },
+    success: function (info) {
+   
+            var htmlstr = template('secondfl',info)
+            $('.xuanran').html(htmlstr)
+    }
+})
+ 
+  $('.changebg').on('click','a',function () {
+    $(this).addClass("current").parent().siblings().find("a").removeClass("current");
+    var id = $(this).data("id");
+    $.ajax({
+        type:'get',
+        url:'/category/querySecondCategory',
+        dataType:'json',
+        data:{
+            id:id
+        },
+        success: function (info) {
+                var htmlstr = template('secondfl',info)
+                $('.xuanran').html(htmlstr)
+        }
+    })
+})
+
+})
